5d241c2a54b51e74e1ded0994d6e13487ee1a407
Abhinandan Aryal <abhi_aryal15@hotmail.com>
2020-01-25 10:08:21 +0545

    Problem 5 initiated

---
+++ project/Problems/Problem5.cs
@@ -0,0 +1,16 @@
+﻿//What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project.Problems
+{
+    public static class Problem5
+    {
+        public static void Execute()
+        {
+
+        }
+    }
+}
